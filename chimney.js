d37c46f59b245f2d479117f92e577d308b8cd4ba
DayTroy <shadowyguy5@gmail.com>
2022-10-26 15:50:53 +0300

    Added some transitions

--- chimney.js
+++ chimney.js
@@ -1,1 +1,11 @@
-const callBlock = document.querySelector('.header-call');
+const consultationFormBtn = document.querySelector('.consultation-form__btn'); 
+const nameForm = document.forms["consultationForm"]["formName"].value;
+const phoneForm = document.forms["consultationForm"]["formPhone"].value;
+consultationFormBtn.addEventListener('click', () => {
+    if(!(nameForm == "" || nameForm == null) && !(phoneForm == "" || phoneForm == null)){
+        alert("Fill out the fields");
+    }
+    return false;
+    //Требуется доработать.
+})
+
